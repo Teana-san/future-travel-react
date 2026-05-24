ab5f65f5ec09b9aaa9b77191ce0a3e25733557be
Tatiana <teana318san@gmail.com>
2026-05-24 20:02:04 +0200

    fix: added vite vercel plugin for spa routing

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import vercel from 'vite-plugin-vercel'
 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
+  plugins: [
+    react(),
     tailwindcss(),
+    vercel(),
   ],
+
+  vercel: {
+    rewrites: [
+      { source: '/(.*)', destination: '/index.html' }
+    ]
+  }
 })
